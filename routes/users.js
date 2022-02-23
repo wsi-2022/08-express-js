913aa00ae9c56768a2527c769244f7375e278e9c
Karl Stolley <karl.stolley@gmail.com>
2022-02-23 16:20:09 -0600

    Add example of req.params and JSON

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:name', function(req, res, next) {
+  const resObj = { name: req.params.name };
+  res.json(resObj);
+})
+
 module.exports = router;
